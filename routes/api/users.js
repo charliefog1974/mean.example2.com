5e6bfede1f5fca11ef7ecd4c7ce64e9148909c17
Vince <charlie_fog@comcast.net>
2020-01-27 20:31:56 -0600

    added post/create

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,24 +2,42 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../../models/users');
 
-router.get('/', function(req, res, next) {
-  Users.find({},function(err, users){
-    if(err){
-     return res.json({'success':false, 'error': err});
-   }
-    return res.json({'success':true, 'users': users});
-  });
+router.get('/', function (req, res, next) {
+    Users.find({}, function (err, users) {
+        if (err) {
+            return res.json({ 'success': false, 'error': err });
+        }
+        return res.json({ 'success': true, 'users': users });
+    });
 });
 
-router.get('/:userId', function(req,res){
-  
+router.get('/:userId', function (req, res) {
+
     var userId = req.params.userId;
-     Users.findOne({'_id':userId}, function(err, user){
-       if(err){
-        return res.json({'success':false, 'error': err});
-      }
-       return res.json({'success':true, 'user': user});
-     });
-   });
+    Users.findOne({ '_id': userId }, function (err, user) {
+        if (err) {
+            return res.json({ 'success': false, 'error': err });
+        }
+        return res.json({ 'success': true, 'user': user });
+    });
+});
+
+router.post('/', function (req, res) {
+    Users.create(new Users({
+        username: req.body.username,
+        email: req.body.email,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    }), function (err, user) {
+
+        if (err) {
+            return res.json({ success: false, user: req.body, error: err });
+        }
+
+        return res.json({ success: true, user: user });
+
+    });
+});
+
 
 module.exports = router;
