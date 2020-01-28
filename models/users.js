19a8faad9b5457f7d59206d9705a9b420cff7eaa
Vince <charlie_fog@comcast.net>
2020-01-27 20:47:01 -0600

    auto-update modified date

--- models/users.js
+++ models/users.js
@@ -4,33 +4,39 @@ var uniqueValidator = require('mongoose-unique-validator');
 
 //Create a schema
 var Users = new Schema({
-  email: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: [true, 'Email must be unique']
-  },
-  username: {
-    type: String,
-    required: [true, 'Please enter a username'],
-    unique: [true, 'Usernames must be unique']
-  },
-  first_name: String,
-  last_name: String,
-  admin: {
-    type: Boolean,
-    default: false
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  modified: {
-    type: Date,
-    default: Date.now
-  }
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: [true, 'Email must be unique']
+    },
+    username: {
+        type: String,
+        required: [true, 'Please enter a username'],
+        unique: [true, 'Usernames must be unique']
+    },
+    first_name: String,
+    last_name: String,
+    admin: {
+        type: Boolean,
+        default: false
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    modified: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 //Add unique validation properties to the model
 Users.plugin(uniqueValidator);
 
-module.exports  = mongoose.model('Users', Users);
+Users.pre('save', function (next) {
+    this.modified = new Date().toISOString();
+    next();
+});
+
+
+module.exports = mongoose.model('Users', Users);
